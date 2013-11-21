8b656bfc248c6c5156ce1089afde4f315066e7ae
Rex <rex@kittykatattack.com>
2013-11-21 16:47:51 -0500

    mostly finished

--- main.js
+++ main.js
@@ -1,42 +1,161 @@
 (function () {
   "use strict";
-  //require(storyMaker.js);
-  
-  var template = "[name] created [an excellent, a very good, a good] one-point perspective imaginary street scene. Her shading skills [are well developed and, are developing and, need practice but] she composed [a highly imaginative, an imaginative, an interesting] family picture. She has [an excellent, a very good, a good] understanding of the elements and principles of design, as exhibited by her art quiz. [name] [produced artwork of a high standard and she has shown an impressive commitment in class, produced good quality work this term and her art skills will continue to improve with practice, shows an ability in art but should remember to maintain focus when in class].";
-  
-  var fairyTale = "[hero] lived in a [place] and was a [prince of the realm, lonely wanderer, skilled magician]. One day he came across [a venemous toad, an evil fairy, a suspicious old crone] and immediately [reached for his sword, fell asleep, wondered what would happen next]. But in the next instant, [hero] felt [a drop of rain, strangely quesy, a small hand tugging at his sleeve] and realized [how bad the situation really was, that he was dreaming, that he had been followed by a dwarf].";
-  
-  //Make a new story like this: `story.make({...options...})`
-  
-  var newStory = story.make({
-    template: fairyTale,
-    keys: {
-      hero: "Rex",
-      place: "palace in the clouds"
-    },
-    //optionNumber: 1,
-    gender: "male"
+  //Define the variables
+  var templateText, templateTextArea, scrollHeight, inputsDiv,
+    keysInTemplate, template, makeStoryButton, newStory,
+    updateTemplateButton, randomRadios, keyInputs,
+    optionInput, optionNumberInput, genders, gender, optionSelected,
+    keyIntructions,
+    keyObject = {};
+
+  //HTML elements
+  templateTextArea = document.querySelector("#template");
+  inputsDiv = document.querySelector("#inputs");
+  makeStoryButton = document.querySelector("#makeStoryButton");
+  newStory = document.querySelector("#newStory");
+  updateTemplateButton = document.querySelector("#updateTemplateButton");
+  randomRadios = document.querySelectorAll("[name=random]");
+  randomRadios = Array.prototype.slice.call(randomRadios);
+  optionInput = document.querySelector("#optionInput");
+  optionNumberInput = document.querySelector("#optionNumberInput");
+  genders = document.querySelectorAll("[name=gender]");
+  genders = Array.prototype.slice.call(genders);
+  keyIntructions = document.querySelector("#keyIntructions");
+
+  //Initilize the template
+  templateText = "[hero] lived in a [place] and was a [prince of the realm, lonely wanderer, skilled magician]. One day he came across [a venomous toad, an evil fairy, a suspicious old crone] and immediately [reached for his sword, fell asleep, wondered what would happen next]. But in the next instant, [hero] felt [a drop of rain, strangely quesy, a small hand tugging at his sleeve] and realized [how bad the situation really was, that he was dreaming, that he had been followed by a dwarf].";
+  //store.clear()
+  loadTemplate();
+
+  function loadTemplate() {
+    //Load the template from local storage if it exists
+    var storedTemplate = store.get("template");
+    //console.log(storedTemplate.name)
+    if (storedTemplate !== undefined) {
+      if (storedTemplate.name !== undefined) {
+        //Retrieve the template and turn <br> line breaks back
+        //into newline characters: \n
+        template = storedTemplate.name.replace(/<br\s*\/?>/mg, "\n");
+      } else {
+        template = templateText;
+      }
+    } else {
+      template = templateText;
+    }
+    console.log(template)
+    templateTextArea.value = template;
+    //Set the scroll height to match the content
+    scrollHeight = templateTextArea.scrollHeight;
+    templateTextArea.style.height = scrollHeight + "px";
+  }
+
+  //Find the keys in the template and display them as
+  //input text field elements
+  findKeysInTemplate();
+  function findKeysInTemplate() {
+    var input, span, p;
+    keysInTemplate = story.findKeysInTemplate(template);
+    if (keysInTemplate !== null) {
+      keyIntructions.style.display = "block";
+      inputsDiv.style.display = "block";
+      keysInTemplate.forEach(function (key) {
+        p = document.createElement("p");
+        inputsDiv.appendChild(p);
+        span = document.createElement("span");
+        span.innerHTML = "[ " + key + " ]";
+        span.setAttribute("class", "label");
+        p.appendChild(span);
+        input = document.createElement("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("placeholder", "Enter something...");
+        p.appendChild(input);
+      });
+      //Get a reference to the key input text fields that were
+      //created in the code above
+      keyInputs = document.querySelectorAll("input[type=text]");
+      keyInputs = Array.prototype.slice.call(keyInputs);
+    } else {
+      keyIntructions.style.display = "none";
+      inputsDiv.style.display = "none";
+    }
+  }
+
+  //The "Make Story" button
+  makeStoryButton.addEventListener("mousedown", makeStoryHandler, false);
+  function makeStoryHandler() {
+    if (templateTextArea.value !== "" || templateTextArea.value !== undefined) {
+      console.log(templateTextArea.value);
+      //Get the keys from the key text input boxes
+      if (keysInTemplate !== null) {
+        keysInTemplate.forEach(function (key, index) {
+          keyObject[key] = keyInputs[index].value;
+        });
+      }
+      //Set the gender for pronouns, if it's been defined
+      genders.some(function (genderRadio) {
+        if (genderRadio.checked) {
+          gender = genderRadio.value;
+          if (gender === "none") {
+            gender = undefined;
+          }
+          return true;
+        }
+      });
+      //Get the text from the template textarea and assign it
+      //to the template variable
+      template = templateTextArea.value;
+      //Preserve line breaks from the textarea. This converts 
+      //Text line break characters to <br>
+      template = template.replace(/\n\r?/g, '<br>');
+      //store the template in local storage
+      //store.set("template", template);
+      store.set("template", {
+        name: template
+      })
+      //Make a new story and assign it to the innerHTML of
+      //the <p> tag on the right side of the screen
+      newStory.innerHTML = story.make({
+        template: template,
+        keys: keyObject,
+        optionNumber: optionSelected,
+        gender: gender
+      });
+    }
+  }
+
+  //The "Update Template" button
+  updateTemplateButton.addEventListener("mousedown", updateTemplateHandler, false);
+  function updateTemplateHandler() {
+    //Remove an previous story key inputs that might 
+    //have been displayed
+    while (inputsDiv.firstChild) {
+      inputsDiv.removeChild(inputsDiv.firstChild);
+    }
+    //Assign the template textarea to the template variable
+    template = templateTextArea.value;
+    //scrollHeight = templateTextArea.scrollHeight;
+    //templateTextArea.style.height = scrollHeight + "px";
+    //Find the new keys in the updated template
+    findKeysInTemplate();
+  }
+
+  //Random option radio buttons
+  randomRadios.forEach(function (radio) {
+    radio.addEventListener("change", randomRadiosHandler, false);
   });
-  
-  //Read the new story
-  console.log(newStory);
-  
-  //Here are the options you can use:
-  //keys: The single words in square brackets from the template. 
-  //For example,  `[name]`, `[place]`, `[villan]`.
-  //Whatever value you give them will be replaced in template by the story maker.
-  //level: Determines which optional text is chosen from the text in
-  //square brackets. For example you might have an option like this:
-  //`[the first one, the second one, the third one.]` If `level` is 2
-  //The this text in the template will be replaced with `the second one`
-  //If you don't inlude a level number, the optional text be random.
-  //gender: changes "he" to "she", "him" to "her" and vice
-  //versa. If you leave this out the gender pronouns won't be changed.
-  
-  //If you want find all the keys in the template, use
-  //`story.findKeysInTemplate`. You could use this to let users
-  //assign their own key values based on a story they've written.
-  var keysUsed = story.findKeysInTemplate(fairyTale);
-  console.log("Keys in template: " + keysUsed.toString());
-  
+  function randomRadiosHandler(event) {
+    if (event.target.value === "off") {
+      optionInput.style.display = "block";
+      optionSelected = optionNumberInput.value;
+    } else {
+      optionInput.style.display = "none";
+      optionSelected = undefined;
+    }
+  }
+
+  //Number option input
+  optionNumberInput.addEventListener("change", optionSelectedHandler, false);
+  function optionSelectedHandler() {
+    optionSelected = optionNumberInput.value;
+  }
 }());
--- storyMaker.js
+++ storyMaker.js
@@ -29,54 +29,61 @@ var story = {
     if (this.gender !== undefined) {
       //Set the correct gender
       if (this.gender.charAt(0) === "b" || this.gender.charAt(0) === "m") {
-        story = story.split("to her").join("to him");
-        story = story.split("her").join("his");
-        story = story.split("Her").join("His");
-        story = story.split("she").join("he");
-        story = story.split("She").join("He");
+        story = story.split(" to her ").join(" to him ");
+        story = story.split(" her ").join(" his ");
+        story = story.split(" Her ").join(" His ");
+        story = story.split(" she ").join(" he ");
+        story = story.split(" She ").join(" He ");
       }
       if (this.gender.charAt(0) === "g" || this.gender.charAt(0) === "f") {
-        story = story.split("his").join("her");
-        story = story.split("His").join("Her");
-        story = story.split("he").join("she");
-        story = story.split("He").join("She");
+        story = story.split(" his ").join(" her ");
+        story = story.split(" His ").join(" Her ");
+        story = story.split(" he ").join(" she ");
+        story = story.split(" He ").join(" She ");
       }
     }
     //Next, set optional text based on the optionNumber number
     //First, turn all the text that's surrounded by square brackets
     //into arrays of strings
     arraysInTemplate = this.getArraysInTemplate(story);
-    stringArrays = this.makeArraysOfStrings(arraysInTemplate);
 
-    //If the stringArrays have more than one element, then we
-    //know that it contains some options
-    stringArrays.forEach(function (stringArray, index) {
-      var originalString, newString, optionToChoose;
-      if (stringArray.length !== 0) {
-        //This is the fun part
-        //a. Get a reference to the original string that's 
-        //surrounded by square brackets
-        originalString = arraysInTemplate[index];
-        //b. Select the element from the new stringArray that matches
-        //the optionNumber (1,2, 3, 4, etc). If the optionNumber is greater than
-        //the number of elements in the string, choose
-        //the last element
-        if (self.optionNumber === undefined) {
-          optionToChoose = self.getRandomOption(stringArray);
-        } else {
-          optionToChoose = self.optionNumber;
+    //Only go further if there are actually arrays in the 
+    //template, otherwise return the story unchanged.
+    if (arraysInTemplate !== null) {
+      stringArrays = this.makeArraysOfStrings(arraysInTemplate);
+
+      //If the stringArrays have more than one element, then we
+      //know that it contains some options
+      stringArrays.forEach(function (stringArray, index) {
+        var originalString, newString, optionToChoose;
+        if (stringArray.length !== 0) {
+          //This is the fun part
+          //a. Get a reference to the original string that's 
+          //surrounded by square brackets
+          originalString = arraysInTemplate[index];
+          //b. Select the element from the new stringArray that matches
+          //the optionNumber (1,2, 3, 4, etc). If the optionNumber is greater than
+          //the number of elements in the string, choose
+          //the last element
+          if (self.optionNumber === undefined) {
+            optionToChoose = self.getRandomOption(stringArray);
+          } else {
+            optionToChoose = self.optionNumber;
+          }
+          if (optionToChoose <= stringArray.length) {
+            newString = stringArray[optionToChoose - 1];
+          } else {
+            newString = stringArray[stringArray.length - 1];
+          }
+          //c. Replace the orginalString with the newString
+          story = story.split(originalString).join(newString);
         }
-        if (optionToChoose <= stringArray.length) {
-          newString = stringArray[optionToChoose - 1];
-        } else {
-          newString = stringArray[stringArray.length - 1];
-        }
-        //c. Replace the orginalString with the newString
-        story = story.split(originalString).join(newString);
-      }
-    });
-    //Return the newly written story
-    return story;
+      });
+      //Return the newly written story
+      return story;
+    } else {
+      return story;
+    }
   },
   //Find all the text in square brackets and convert them to arrays
   getArraysInTemplate: function (story) {
@@ -93,7 +100,7 @@ var story = {
     "use strict";
     var stringArrays = [];
 
-    //2. Convert each submatch into an array of strings
+    //Convert each submatch into an array of strings
     stringArrays = matches.map(function (match) {
       match = match.slice(1, -1).split(/\s*,\s*/);
       return match;
@@ -106,17 +113,25 @@ var story = {
   //words or phrases separated by commas.) 
   findKeysInTemplate: function (template) {
     "use strict";
-    var arraysInTemplate = this.getArraysInTemplate(template);
-    var keysInTemplate = arraysInTemplate.filter(function (string) {
-      if (string.indexOf(",") === -1) {
-        return string;
-      }
-    });
-    //Remove duplicates
-    keysInTemplate = keysInTemplate.filter(function (key, index, self) {
-      return self.indexOf(key) === index;
-    });
-    return keysInTemplate;
+    //Find single words surrounded by square brackets
+    var keysInTemplate = template.match(/\[(\S*)\]/g);
+    
+    //Only proceed if there actually are keys in the template
+    if (keysInTemplate !== null) {
+      //Remove duplicates
+      keysInTemplate = keysInTemplate.filter(function (key, index, self) {
+        return self.indexOf(key) === index;
+      });
+      //Remove the square brackets from the strings
+      keysInTemplate = keysInTemplate.map(function (newString) {
+        newString = newString.replace(/[\[\]']+/g, "");
+        return newString;
+      });
+      console.log(keysInTemplate);
+      return keysInTemplate;
+    } else {
+      return keysInTemplate;
+    }
   },
   //If optionNumber isn't defined, return a random number 
   //between 1 and the option array with the 
